4b2f5048730de08bca3d98e94f85d769d721ea5f
jtchecchi <jtchecchi@users.noreply.github.com>
2014-09-13 18:17:56 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Examples of Pets";
+     $array = array("Dog" , "Cat" , "Mouse" , "Lizard" , "Hamster" , "Snake");
+      echo $array[1];
+      break;
+      echo $array[2];
+?>
+
+</body>
+</html> 
